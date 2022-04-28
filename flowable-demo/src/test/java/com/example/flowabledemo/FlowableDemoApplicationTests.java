126793a37a8a970005428c78277be5f1656e4bf0
nicol <nicolas.kilchenmann@bluewin.ch>
2022-04-28 18:31:15 +0200

    overall improvements

--- flowable-demo/src/test/java/com/example/flowabledemo/FlowableDemoApplicationTests.java
+++ flowable-demo/src/test/java/com/example/flowabledemo/FlowableDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.flowabledemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,21 @@ class FlowableDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dummyTestIntegers(){
+		Assertions.assertEquals(5,5,"Dummy assertion integers");
+	}
+
+	@Test
+	void dummyTestStrings(){
+		Assertions.assertNotEquals("hallo","velo","Dummy assertion test strings");
+	}
+
+	@Test
+	void dummyTestBooleans(){
+		Assertions.assertTrue(true,"Dummy assertion test booleans");
 	}
 
 }
